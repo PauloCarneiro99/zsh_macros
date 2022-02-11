8571e8c4c9e9712118d0c3d7a617a64b91485109
paulocarneiro99 <paulo.carneiro@quintoandar.com.br>
2022-02-11 10:53:49 -0300

    updating setup shell script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-cp -r .my_zsh_macros/ ~/
-echo "fpath=( ~/.my_zsh_macros "${fpath[@]}" )" >> ~/.zshrc
-echo "autoload -U $fpath[1]/*(.:t)/n" >> ~/.zshrc
+cp -r .my_zsh_macros/ ~/.my_zsh_macros/
+echo 'fpath=( ~/.my_zsh_macros "${fpath[@]}" )' >> ~/.zshrc
+echo 'autoload -U $fpath[1]/*(.:t)' >> ~/.zshrc
